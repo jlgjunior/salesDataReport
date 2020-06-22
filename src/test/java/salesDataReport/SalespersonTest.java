404735316e3143ff7457201639392a54fbe2f6f2
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-22 15:41:54 -0300

    Modify test to use a Salesperson object

--- src/test/java/salesDataReport/SalespersonTest.java
+++ src/test/java/salesDataReport/SalespersonTest.java
@@ -5,12 +5,13 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import models.Person;
+import models.Salesperson;
 
 public class SalespersonTest {
 
 	@Test
 	public void getNameTest() {
-		Person person = new Person("John");
+		Person person = new Salesperson("John");
 		assertEquals("John", person.getName());
 	}
 }
