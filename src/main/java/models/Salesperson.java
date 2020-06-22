7518b244bcee1da74d2ed4e131c0ec0d08f62ef2
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-22 20:40:14 -0300

    Add sales attribute and getSales method to Salesperson

--- src/main/java/models/Salesperson.java
+++ src/main/java/models/Salesperson.java
@@ -1,9 +1,18 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Salesperson extends Person {
+
+	private List<Object> sales = new ArrayList();
 
 	public Salesperson(String name) {
 		super(name);
+	}
+
+	public List<Object> getSales() {
+		return this.sales ;
 	}
 
 }
