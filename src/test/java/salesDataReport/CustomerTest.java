69c44c09c7ba8773e8bb0fd262f3d56ee615d1d3
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-22 18:34:03 -0300

    Add new test for costumer

---
+++ src/test/java/salesDataReport/CustomerTest.java
@@ -0,0 +1,19 @@
+package salesDataReport;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import models.Person;
+
+
+public class CustomerTest {
+
+	@Test
+	public void getNameTest() {
+		String name = "Ed";
+		Person person = new Customer(name);
+		assertEquals(name, person.getName());
+	}
+	
+}
