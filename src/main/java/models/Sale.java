89f106368ad528ba860a3009dc1b69f87f3f9965
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-23 11:06:30 -0300

    Add products attribtue to Sale and creates getProducts method

--- src/main/java/models/Sale.java
+++ src/main/java/models/Sale.java
@@ -1,8 +1,12 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sale {
 	
 	private Integer id;
+	private List<Product> products = new ArrayList<Product>();
 
 	public Sale(Integer id) {
 		this.id = id;
@@ -10,6 +14,10 @@ public class Sale {
 	
 	public Integer getId() {
 		return this.id;
+	}
+
+	public List<Product> getProducts() {
+		return this.products ;
 	}
 
 }
