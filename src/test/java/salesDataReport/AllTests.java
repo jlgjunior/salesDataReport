731cb10ba4ec376ee613e11822753226b07422fb
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-23 09:47:08 -0300

    Add test suit

---
+++ src/test/java/salesDataReport/AllTests.java
@@ -0,0 +1,11 @@
+package salesDataReport;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CustomerTest.class, SalespersonTest.class, SaleTest.class })
+public class AllTests {
+
+}
