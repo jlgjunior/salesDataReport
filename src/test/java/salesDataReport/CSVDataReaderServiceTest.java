d836f7f762a82648e1b2d8bff5d77d0cd31a4c34
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-24 11:29:24 -0300

    Update CVSDataReaderServiceTest to include a data loading test and an example file

--- src/test/java/salesDataReport/CSVDataReaderServiceTest.java
+++ src/test/java/salesDataReport/CSVDataReaderServiceTest.java
@@ -2,12 +2,20 @@ package salesDataReport;
 
 import static org.junit.Assert.*;
 
+import java.io.FileReader;
+import java.io.Reader;
+
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import services.CSVDataReaderService;
 
+
 public class CSVDataReaderServiceTest {
 
+	@Autowired
+	CSVDataReaderService csvDataReaderService;
+	
 	@Test
 	public void initTest() {
 		try {
@@ -16,7 +24,17 @@ public class CSVDataReaderServiceTest {
 		catch (Exception e){
 			fail(e.getMessage());
 		}
-		
+	}
+	
+	@Test
+	public void loadDataTest() {
+		try {
+			Reader reader = new FileReader("example.txt");
+			this.csvDataReaderService.loadData(reader);
+		}
+		catch (Exception e){
+			fail(e.getMessage());
+		}
 	}
 
 }
