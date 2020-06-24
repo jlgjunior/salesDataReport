24fffb9d7e82945887810cb6feaa697c229cbc5f
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-24 12:32:44 -0300

    Creates method loadData

--- src/main/java/services/CSVDataReaderService.java
+++ src/main/java/services/CSVDataReaderService.java
@@ -1,15 +1,29 @@
 package services;
 
 import java.io.Reader;
+import java.util.List;
 
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 
 public class CSVDataReaderService {
 
 	
 	public CSVDataReaderService() {
-	
+	 
+	}
 
+	public List<String[]> loadData(Reader reader) {
+		CSVParser csvParser = new CSVParserBuilder().withSeparator('ç')
+				                          			.withIgnoreQuotations(true)
+				                          			.build();
+		CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(0)
+														  .withCSVParser(csvParser)
+														  .build();
+		
+		return csvReader.readAll();
 	}
 
 }
