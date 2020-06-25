34037352c4f27eca54a16a2f5e849708e5370b02
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-25 19:03:41 -0300

    Update CSVSalespersonTest.getNameTest method

--- src/main/java/models/CSVSalespersonData.java
+++ src/main/java/models/CSVSalespersonData.java
@@ -23,4 +23,40 @@ public class CSVSalespersonData extends CSVData {
 		this.salary = Float.valueOf(salary);
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + Float.floatToIntBits(salary);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CSVSalespersonData other = (CSVSalespersonData) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Float.floatToIntBits(salary) != Float.floatToIntBits(other.salary))
+			return false;
+		return true;
+	}
+	
+	
+
 }
--- src/test/java/salesDataReport/CSVSalespersonDataTest.java
+++ src/test/java/salesDataReport/CSVSalespersonDataTest.java
@@ -1,21 +1,41 @@
 package salesDataReport;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+
 import org.junit.Test;
 
+import models.CSVData;
 import models.CSVSalespersonData;
+import services.CSVDataReaderService;
 
 
 public class CSVSalespersonDataTest {
 
 	@Test
 	public void getNameTest() {
-		CSVSalespersonData csvSalespersonData;
-		csvSalespersonData = 
-		assertTrue();
+		String filename;
+		Reader reader;
+		CSVSalespersonData resultData = null;
+		CSVSalespersonData salespersonData = new CSVSalespersonData();
+	    salespersonData 
+		     .loadData("1234567891234", "Pedro", "50000");
+		filename = getClass().getClassLoader().getResource("example").getFile();
+		CSVDataReaderService csvDataReaderService = new CSVDataReaderService();
+		try {
+			reader = new FileReader(filename);
+			csvDataReaderService.loadData(reader);
+			resultData = (CSVSalespersonData) csvDataReaderService.getNextCSVData();
+			assertEquals(salespersonData, resultData);
+		} catch (FileNotFoundException e) {
+			fail(e.getMessage());
+		}
 	}
 	
 }
