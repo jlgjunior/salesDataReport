9c7683bf9c07ab39be3690d8567276902ea404d7
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-26 08:24:39 -0300

    Refactor CSVDataSale class. Add getters for private fields

--- src/main/java/models/CSVSaleData.java
+++ src/main/java/models/CSVSaleData.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.Arrays;
+
 public class CSVSaleData implements CSVData {
 
 	private Integer id;
@@ -23,6 +25,52 @@ public class CSVSaleData implements CSVData {
 		catch (ArrayIndexOutOfBoundsException e) {
 			return false;
 		}
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String[] getProducts() {
+		return products;
+	}
+
+	public String getSalespersonName() {
+		return salespersonName;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + Arrays.hashCode(products);
+		result = prime * result + ((salespersonName == null) ? 0 : salespersonName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CSVSaleData other = (CSVSaleData) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (!Arrays.equals(products, other.products))
+			return false;
+		if (salespersonName == null) {
+			if (other.salespersonName != null)
+				return false;
+		} else if (!salespersonName.equals(other.salespersonName))
+			return false;
+		return true;
 	}
 
 }
