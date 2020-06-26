86485a517a4245ba64316ed40fcc7195df488f9f
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-26 10:17:02 -0300

    Refactor createCSVDataTest. Now failing

--- src/test/java/salesDataReport/CSVDataFactoryTest.java
+++ src/test/java/salesDataReport/CSVDataFactoryTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
+import models.CSVCustomerData;
+import models.CSVSaleData;
 import models.CSVSalespersonData;
 import services.CSVDataReaderService;
 
@@ -16,6 +18,11 @@ public class CSVDataFactoryTest {
 		CSVDataReaderService csvDataReaderService = new CSVDataReaderService();
 		if (csvDataReaderService.loadCSVFile(filename)) {
 			assertTrue(csvDataReaderService.getNextCSVData() instanceof CSVSalespersonData);
+			assertTrue(csvDataReaderService.getNextCSVData() instanceof CSVSalespersonData);
+			assertTrue(csvDataReaderService.getNextCSVData() instanceof CSVCustomerData);
+			assertTrue(csvDataReaderService.getNextCSVData() instanceof CSVCustomerData);
+			assertTrue(csvDataReaderService.getNextCSVData() instanceof CSVSaleData);
+			assertTrue(csvDataReaderService.getNextCSVData() instanceof CSVSaleData);
 		}
 		else {
 			fail("Could not load csv file");
