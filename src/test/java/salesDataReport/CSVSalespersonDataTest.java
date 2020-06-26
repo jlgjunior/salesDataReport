36d06054e543e32bbd8815b130c188977b25ec48
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-26 08:50:06 -0300

    Create tests for CSVSalespersonData tests

--- src/test/java/salesDataReport/CSVSalespersonDataTest.java
+++ src/test/java/salesDataReport/CSVSalespersonDataTest.java
@@ -1,5 +1,6 @@
 package salesDataReport;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -14,6 +15,33 @@ public class CSVSalespersonDataTest {
 		String[] data = {"001", "1234567891234", "Pedro", "50000"};
 		CSVData salespersonData = new CSVSalespersonData();
 		assertTrue(salespersonData.loadData(data));
+	}
+	
+	@Test
+	public void getCpfTest() {
+		final String CPF = "1234567891234";
+		String[] data = {"001", CPF, "Pedro", "50000"};
+		CSVSalespersonData salespersonData = new CSVSalespersonData();
+		salespersonData.loadData(data);
+		assertEquals(CPF, salespersonData.getCpf());
+	}
+	
+	@Test
+	public void getNameTest() {
+		final String NAME = "Pedro";
+		String[] data = {"001", "1234567891234", NAME, "50000"};
+		CSVSalespersonData salespersonData = new CSVSalespersonData();
+		salespersonData.loadData(data);
+		assertEquals(NAME, salespersonData.getName());
+	}
+	
+	@Test
+	public void getSalaryTest() {
+		final Float SALARY = 50000f;
+		String[] data = {"001", "1234567891234", "Pedro", "50000"};
+		CSVSalespersonData salespersonData = new CSVSalespersonData();
+		salespersonData.loadData(data);
+		assertEquals(SALARY, salespersonData.getSalary());
 	}
 	
 }
