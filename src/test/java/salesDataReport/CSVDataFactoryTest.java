cdd1c13e9461d5e82b8b4abbf747ffa52839cf81
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-26 10:01:37 -0300

    New test class: CSVDataFactoryTest

---
+++ src/test/java/salesDataReport/CSVDataFactoryTest.java
@@ -0,0 +1,15 @@
+package salesDataReport;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class CSVDataFactoryTest {
+	
+	@Test
+	public void createCSVDataTest() {
+		String filename = getClass().getClassLoader().getResource("exampleSimple").getFile();
+		fail("Fail");
+	}
+	
+}
