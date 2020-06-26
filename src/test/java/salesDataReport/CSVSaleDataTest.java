3694da0dead40eed12fafd7d946de96aeee3de00
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-26 08:40:54 -0300

    Tests for CSVSaleData passing

--- src/test/java/salesDataReport/CSVSaleDataTest.java
+++ src/test/java/salesDataReport/CSVSaleDataTest.java
@@ -1,5 +1,7 @@
 package salesDataReport;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -13,8 +15,38 @@ public class CSVSaleDataTest {
 	public void loadDataTest() {
 		String[] data = {"003", "10", 
 				         "[1-10-100,2-30-2.50,3-40-3.10]", "Pedro"};
-		CSVData salespersonData = new CSVSaleData();
-		assertTrue(salespersonData.loadData(data));
+		CSVData saleData = new CSVSaleData();
+		assertTrue(saleData.loadData(data));
+	}
+
+	@Test
+	public void getSalespersonNameTest() {
+		final String SALESPERSON_NAME = "Pedro";
+		String[] data = {"003", "10", 
+				         "[1-10-100,2-30-2.50,3-40-3.10]", SALESPERSON_NAME};
+		CSVSaleData saleData = new CSVSaleData();
+		saleData.loadData(data);
+		assertEquals(SALESPERSON_NAME, saleData.getSalespersonName());
+	}
+
+	@Test
+	public void getIdTest() {
+		final Integer ID = 10;
+		String[] data = {"003", "10", 
+				         "[1-10-100,2-30-2.50,3-40-3.10]", "Pedro"};
+		CSVSaleData saleData = new CSVSaleData();
+		saleData.loadData(data);
+		assertEquals(ID, saleData.getId());
+	}
+
+	@Test
+	public void getProductsTest() {
+		final String[] PRODUCTS = {"1-10-100","2-30-2.50","3-40-3.10"};
+		String[] data = {"003", "10", 
+				         "[1-10-100,2-30-2.50,3-40-3.10]", "Pedro"};
+		CSVSaleData saleData = new CSVSaleData();
+		saleData.loadData(data);
+		assertArrayEquals(PRODUCTS, saleData.getProducts());
 	}
 	
 }
