56bb44e52cf9697aae3f5ef1c8265a033766e8b7
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-27 00:19:04 -0300

    Refactor Customer test adding loadCSVDataTest

--- src/test/java/salesDataReport/CustomerTest.java
+++ src/test/java/salesDataReport/CustomerTest.java
@@ -1,6 +1,8 @@
 package salesDataReport;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import models.CSVCustomerData;
@@ -18,6 +20,24 @@ public class CustomerTest {
 		Person customer = new Customer();
 		ReflectionTestUtils.setField(customer, "name", NAME);
 		assertEquals(NAME, customer.getName());
+	}
+	
+	@Test
+	public void loadDataTest() {
+		final String NAME = "Ed";
+		final String CNPJ = "10";
+		final String AREA = "Games";
+		boolean result;
+		Customer customer = new Customer();
+		CSVCustomerData csvData = new CSVCustomerData();
+		ReflectionTestUtils.setField(csvData, "name", NAME);
+		ReflectionTestUtils.setField(csvData, "cnpj", CNPJ);
+		ReflectionTestUtils.setField(csvData, "area", AREA);
+		customer.loadCSVData(csvData);
+		result = csvData.getName().equals(customer.getName());
+		result &= csvData.getCnpj().equals(customer.getCnpj());
+		result &= csvData.getArea().equals(customer.getArea());
+		assertTrue(result);
 	}
 	
 }
