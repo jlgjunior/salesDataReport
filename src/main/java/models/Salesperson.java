1e0dceff23237e7ef32a901baaa48c4812bb29e0
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-27 00:29:10 -0300

    Generate equals and hashCode

--- src/main/java/models/Salesperson.java
+++ src/main/java/models/Salesperson.java
@@ -36,4 +36,41 @@ public class Salesperson extends Person implements PersistantModel<CSVSalesperso
 		salary = data.getSalary();
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((salary == null) ? 0 : salary.hashCode());
+		result = prime * result + ((sales == null) ? 0 : sales.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Salesperson other = (Salesperson) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (salary == null) {
+			if (other.salary != null)
+				return false;
+		} else if (!salary.equals(other.salary))
+			return false;
+		if (sales == null) {
+			if (other.sales != null)
+				return false;
+		} else if (!sales.equals(other.sales))
+			return false;
+		return true;
+	}
+
 }
