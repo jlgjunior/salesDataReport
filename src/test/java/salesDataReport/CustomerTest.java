998cf10999eedeee5adfcc29489ef6968cfd44a4
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-26 23:04:24 -0300

    Refactor CutomerTest.getNameTest

--- src/test/java/salesDataReport/CustomerTest.java
+++ src/test/java/salesDataReport/CustomerTest.java
@@ -2,17 +2,22 @@ package salesDataReport;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+
+import models.CSVCustomerData;
+import models.CSVData;
 import models.Customer;
 import models.Person;
-
+import org.junit.platform.commons.util.ReflectionUtils;
+import org.springframework.test.util.ReflectionTestUtils;
 
 public class CustomerTest {
 
 	@Test
 	public void getNameTest() {
-		String name = "Ed";
-		Person customer = new Customer(name);
-		assertEquals(name, customer.getName());
+		final String NAME = "Ed";
+		Person customer = new Customer();
+		ReflectionTestUtils.setField(customer, "name", NAME);
+		assertEquals(NAME, customer.getName());
 	}
 	
 }
