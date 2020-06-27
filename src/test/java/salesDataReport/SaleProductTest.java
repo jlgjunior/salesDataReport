9fe107318bcd1964203b550847e50e04234f5a8b
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-27 17:56:25 -0300

    Remove SaleProductTest warnings

--- src/test/java/salesDataReport/SaleProductTest.java
+++ src/test/java/salesDataReport/SaleProductTest.java
@@ -1,15 +1,10 @@
 package salesDataReport;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import models.PersistantModel;
-import models.Product;
-import models.Sale;
 import models.SaleProduct;
 
 public class SaleProductTest {
