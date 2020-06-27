14bb715b7190760ece94215c87de60f8e2c0f3e1
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-27 17:30:24 -0300

    Add new attributes and mofify constructor

--- src/main/java/models/Product.java
+++ src/main/java/models/Product.java
@@ -3,13 +3,17 @@ package models;
 public class Product {
 
 	private Integer id;
+	private Sale sale;
 	
-	public Product(Integer id) {
-		this.id = id;
+	public Product() {
+		
 	}
 
 	public Integer getId() {
 		return this.id;
 	}
-	
+
+	public Sale getSale() {
+		return this.sale;
+	}
 }
