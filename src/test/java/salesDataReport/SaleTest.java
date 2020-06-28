1a02ea14f6df740e605ac2bdc44cdffd34f9cae1
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-28 12:02:11 -0300

    Fix SaleTest

--- src/test/java/salesDataReport/SaleTest.java
+++ src/test/java/salesDataReport/SaleTest.java
@@ -1,13 +1,8 @@
 package salesDataReport;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import org.springframework.test.util.ReflectionTestUtils;
-
-import models.Product;
 import models.Sale;
 
 public class SaleTest {
@@ -18,27 +13,4 @@ public class SaleTest {
 		Sale sale = new Sale();
 		assertEquals(ID, sale.getId());
 	}
-	
-	@Test
-	//checks if an empty sales product list is instantiated
-	//upon sale creation
-	public void getProductsEmptyTest() {
-		final Integer ID = 148;
-		Sale sale = new Sale();
-		ReflectionTestUtils.setField(sale, "id", ID);
-		assertTrue(sale.getProducts().isEmpty());
-	}
-	
-	@Test
-	//checks if an products object is a product
-	public void addProductTest() {
-		final Integer ID = 148;
-		Sale sale = new Sale();
-		ReflectionTestUtils.setField(sale, "id", ID);
-		Product product = new Product();
-		ReflectionTestUtils.setField(product, "id", ID);
-		sale.addProduct(product);
-		assertSame(sale.getProducts().get(0), product);
-	}
-	
 }
