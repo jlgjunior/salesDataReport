bc33c9b697f7fbdce0686db9d208202e1ee5d4ca
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-28 08:32:41 -0300

    Add Customer creation to DataImporterService

--- src/main/java/services/DataImporterService.java
+++ src/main/java/services/DataImporterService.java
@@ -2,7 +2,9 @@ package services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import builders.CustomerBuilder;
 import builders.SalespersonBuilder;
+import models.CSVCustomerData;
 import models.CSVData;
 import models.CSVSalespersonData;
 import models.PersistantModel;
@@ -19,16 +21,26 @@ public class DataImporterService {
 	public void importDataFromFile(String filepath) {
 		csvReaderService.loadCSVFile(filepath);
 		while(csvReaderService.hasNextData()) {
+			PersistantModel model;
 			CSVData csvData = csvReaderService.getNextCSVData();
 			switch(csvData.getCode()) {
 				case "001":
 					CSVSalespersonData salespersonData = 
 							(CSVSalespersonData) csvData;
-					PersistantModel model = new SalespersonBuilder()
-											.setCpf(salespersonData.getCpf())
-											.setName(salespersonData.getName())			   
-											.setSalary(salespersonData.getSalary())
-											.build();
+					model = new SalespersonBuilder()
+								.setCpf(salespersonData.getCpf())
+								.setName(salespersonData.getName())			   
+								.setSalary(salespersonData.getSalary())
+								.build();
+					break;
+				case "002":
+					CSVCustomerData customerData = 
+							(CSVCustomerData) csvData;
+					model = new CustomerBuilder()
+								.setCnpj(customerData.getCnpj())
+								.setName(customerData.getName())			   
+								.setArea(customerData.getArea())
+								.build();
 					break;
 			}	
 		}
