ef386982d6dd633ef84a9cbe8a0f2f22e7203b55
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-28 09:54:45 -0300

    Update Sale creating new constructor to receive builder as parameter

--- src/main/java/models/Sale.java
+++ src/main/java/models/Sale.java
@@ -3,6 +3,8 @@ package models;
 import java.util.ArrayList;
 import java.util.List;
 
+import builders.SaleBuilder;
+
 public class Sale {
 	
 	private Integer id;
@@ -12,6 +14,10 @@ public class Sale {
 		
 	}
 	
+	public Sale(SaleBuilder saleBuilder) {
+		id = saleBuilder.getId();
+	}
+
 	public Integer getId() {
 		return this.id;
 	}
@@ -24,4 +30,34 @@ public class Sale {
 		this.products.add(product);
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((products == null) ? 0 : products.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sale other = (Sale) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (products == null) {
+			if (other.products != null)
+				return false;
+		} else if (!products.equals(other.products))
+			return false;
+		return true;
+	}
 }
