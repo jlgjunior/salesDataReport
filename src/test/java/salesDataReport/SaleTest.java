f6fbf091c420066f6fec6402e9b050d476cae51e
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-28 18:12:15 -0300

    Update tests. Ignoring DataImporterServiceTest for a while

--- src/test/java/salesDataReport/SaleTest.java
+++ src/test/java/salesDataReport/SaleTest.java
@@ -3,6 +3,8 @@ package salesDataReport;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
 import models.Sale;
 
 public class SaleTest {
@@ -11,6 +13,7 @@ public class SaleTest {
 	public void getIdTest() {
 		final Integer ID = 148;
 		Sale sale = new Sale();
+		ReflectionTestUtils.setField(sale, "id", ID);
 		assertEquals(ID, sale.getId());
 	}
 }
