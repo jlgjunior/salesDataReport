eec28595256a9609c1400f9c0a5507be1aad7990
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-28 10:35:55 -0300

    Add hash and equal methods, and add price attribute to Product

--- src/main/java/models/Product.java
+++ src/main/java/models/Product.java
@@ -4,6 +4,7 @@ public class Product {
 
 	private Integer id;
 	private Sale sale;
+	private Float price;
 	
 	public Product() {
 		
@@ -15,5 +16,46 @@ public class Product {
 
 	public Sale getSale() {
 		return this.sale;
+	}
+
+	public Float getPrice() {
+		return price;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((price == null) ? 0 : price.hashCode());
+		result = prime * result + ((sale == null) ? 0 : sale.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (price == null) {
+			if (other.price != null)
+				return false;
+		} else if (!price.equals(other.price))
+			return false;
+		if (sale == null) {
+			if (other.sale != null)
+				return false;
+		} else if (!sale.equals(other.sale))
+			return false;
+		return true;
 	}
 }
