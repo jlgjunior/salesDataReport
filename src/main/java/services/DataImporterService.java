055ea0e93ca3137f069db756318647acd947bde5
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-28 00:18:26 -0300

    In dev importDataFromFile

--- src/main/java/services/DataImporterService.java
+++ src/main/java/services/DataImporterService.java
@@ -2,6 +2,10 @@ package services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import models.CSVData;
+import models.CSVSalespersonData;
+import models.PersistantModel;
+
 public class DataImporterService {
 
 	@Autowired
@@ -9,6 +13,24 @@ public class DataImporterService {
 	
 	public DataImporterService() {
 		
+	}
+
+	public void importDataFromFile(String filepath) {
+		csvReaderService.loadCSVFile(filepath);
+		while(csvReaderService.hasNextData()) {
+			CSVData csvData = csvReaderService.getNextCSVData();
+			switch(csvData.getCode()) {
+				case "001":
+					CSVSalespersonData salespersonData = 
+							(CSVSalespersonData) csvData;
+					PersistantModel model = new SalespersonBuilder()
+											.setCpf(salespersonData.getCpf())
+											.setName(salespersonData.getName())			   
+											.setSalary(salespersonData.getSalary())
+											.build();
+					break;
+			}	
+		}
 	}
 	
 }
