a04cdeabfd777a800f04a4e79d3dc3ba3fa1b0c4
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-28 10:57:44 -0300

    Removes price from Product

--- src/main/java/models/Product.java
+++ src/main/java/models/Product.java
@@ -4,7 +4,6 @@ public class Product {
 
 	private Integer id;
 	private Sale sale;
-	private Float price;
 	
 	public Product() {
 		
@@ -18,16 +17,11 @@ public class Product {
 		return this.sale;
 	}
 
-	public Float getPrice() {
-		return price;
-	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((price == null) ? 0 : price.hashCode());
 		result = prime * result + ((sale == null) ? 0 : sale.hashCode());
 		return result;
 	}
@@ -45,11 +39,6 @@ public class Product {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
-			return false;
-		if (price == null) {
-			if (other.price != null)
-				return false;
-		} else if (!price.equals(other.price))
 			return false;
 		if (sale == null) {
 			if (other.sale != null)
