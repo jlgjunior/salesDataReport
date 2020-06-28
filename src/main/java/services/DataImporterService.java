1cb7c53df7952cfae7c9b94246a849b608e8a17e
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-28 10:00:35 -0300

    Refactor importDataFromFile to user only factories

--- src/main/java/services/DataImporterService.java
+++ src/main/java/services/DataImporterService.java
@@ -2,11 +2,8 @@ package services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import builders.CustomerBuilder;
-import builders.SalespersonBuilder;
-import models.CSVCustomerData;
+import factories.PersistantModelFactory;
 import models.CSVData;
-import models.CSVSalespersonData;
 import models.PersistantModel;
 
 public class DataImporterService {
@@ -23,35 +20,9 @@ public class DataImporterService {
 		while(csvReaderService.hasNextData()) {
 			PersistantModel model;
 			CSVData csvData = csvReaderService.getNextCSVData();
-			switch(csvData.getCode()) {
-				case "001":
-					CSVSalespersonData salespersonData = 
-							(CSVSalespersonData) csvData;
-					model = new SalespersonBuilder()
-								.setCpf(salespersonData.getCpf())
-								.setName(salespersonData.getName())			   
-								.setSalary(salespersonData.getSalary())
-								.build();
-					break;
-				case "002":
-					CSVCustomerData customerData = 
-							(CSVCustomerData) csvData;
-					model = new CustomerBuilder()
-								.setCnpj(customerData.getCnpj())
-								.setName(customerData.getName())			   
-								.setArea(customerData.getArea())
-								.build();
-					break;
-				case "003":
-					CSVCustomerData customerData = 
-							(CSVCustomerData) csvData;
-					model = new CustomerBuilder()
-								.setCnpj(customerData.getCnpj())
-								.setName(customerData.getName())			   
-								.setArea(customerData.getArea())
-								.build();
-					break;
-			}	
+			model = PersistantModelFactory
+					.getConcreteFactory(csvData)
+					.createPersistantModel(csvData);
 		}
 	}
 	
