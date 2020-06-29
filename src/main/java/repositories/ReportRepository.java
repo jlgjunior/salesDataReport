d70e62ac46b1b17dc6df88c8c8f9dc0e7da66416
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-29 10:46:30 -0300

    New method saveToFile in ReportRepository

--- src/main/java/repositories/ReportRepository.java
+++ src/main/java/repositories/ReportRepository.java
@@ -1,17 +1,41 @@
 package repositories;
 
+import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.SortedSet;
 
-import models.Report;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import models.Report;
+import services.FileSystemManagerService;
+
+@Repository
 public class ReportRepository implements IRepository<Report> {
 
+	@Autowired
+	private FileSystemManagerService fileSystemManagerService;
+	
 	@Override
 	public Report save(Report entity) {
-		// TODO Auto-generated method stub
-		return null;
+		return entity;
 	}
 
+	public Report saveToFile(Report entity, String filename) {
+		String data = entity.toString();
+		String format = "yyyyMMddHHmmssSSS'.txt'";
+		filename += new SimpleDateFormat(format).format(new Date());
+		Path fileFullPath = fileSystemManagerService
+									.getOutputFolder();
+		fileFullPath = fileSystemManagerService
+				            .combinePath(fileFullPath.toString(), filename);
+		fileSystemManagerService
+		        .writeTextFile(fileFullPath.toString(), data);
+		return save(entity);
+		
+	}
+	
 	@Override
 	public SortedSet<Report> findAll() {
 		return null;
