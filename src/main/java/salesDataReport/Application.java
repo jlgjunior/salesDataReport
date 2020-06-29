b5458a28d02031f3f87603db09d022045a406108
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-29 13:42:32 -0300

    Create new test for application

--- src/main/java/salesDataReport/Application.java
+++ src/main/java/salesDataReport/Application.java
@@ -1,10 +1,13 @@
 package salesDataReport;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
 
 import services.FileWatcherService;
 
+@ComponentScan({"services", "models", "repositories", "factories"})
 public class Application {
 	@Autowired
 	FileWatcherService fileWatcherService;
