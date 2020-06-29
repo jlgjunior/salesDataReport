993a1967b6a412f55f20bd70301a46c9b004605a
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-29 12:54:20 -0300

    Call process on main

--- src/main/java/salesDataReport/Application.java
+++ src/main/java/salesDataReport/Application.java
@@ -1,15 +1,20 @@
 package salesDataReport;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-public class Application {
+import services.FileWatcherService;
 
+public class Application {
+	@Autowired
+	FileWatcherService fileWatcherService;
+	
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class).headless(false).run(args);
 	}
 	
 	public void run(String... args) throws Exception {
-	
+		fileWatcherService.watch();
 	}
 
 }
