0efeded1455f1848ee3ffd84fc12d63ffd5915b8
jlgjunior <jgaribaldijunior@gmail.com>
2020-06-29 08:45:17 -0300

    New ReportService class

---
+++ src/main/java/services/ReportService.java
@@ -0,0 +1,72 @@
+package services;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.SortedSet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import builders.ReportBuilder;
+import models.Customer;
+import models.Report;
+import models.Sale;
+import models.Salesperson;
+import repositories.IRepository;
+
+@Service
+public class ReportService {
+
+	private Path outputFolder;
+	@Autowired
+	private DataImporterService dataImporterService;
+	@Autowired
+	CSVDataReaderService csvReaderService;
+	@Autowired
+	IRepository<Salesperson> salespersonRepository;
+	@Autowired
+	IRepository<Customer> customerRepository;
+	@Autowired
+	IRepository<Sale> saleRepository;
+	@Autowired
+	IRepository<Report> reportRepository;
+	
+	public ReportService() {
+		String home = System.getProperty("user.home");
+		outputFolder = Paths.get(home, "out");
+		try {
+			Files.createDirectories(outputFolder);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("System initialization error due to"
+					+ " I/O exception. Error in report service");
+		}
+	}
+	
+	public void generateReport(String filepath) {
+		dataImporterService.importDataFromFile(filepath);
+		SortedSet<Salesperson> salespeople =
+								salespersonRepository.findAll();
+		
+		Integer salespeopleCount = salespeople.size();
+		Integer customersCount = customerRepository
+											.findAll().size();
+		
+		SortedSet<Sale> sales = saleRepository.findAll();
+		Long mostExpensiveSaleId = 
+				sales.size() > 0? sales.first().getId() : null;
+
+		String worstSalespersonName = 
+				salespeople.size() > 0? salespeople.first().getName() : null;
+	
+		Report report = new ReportBuilder()
+							.setCustomersCount(customersCount)
+							.setSalespeopleCount(salespeopleCount)
+							.setMostExpensiveSaleId(mostExpensiveSaleId)
+							.setWorstSalespersonName(worstSalespersonName)
+							.build();
+		reportRepository.save(report);
+	}										
+}
